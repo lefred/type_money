898402a79256d5e8ad976275e7add49bd4549dba
Frederic Descamps <lefred@mariadb.org>
2026-04-14 20:23:38 +0200

    conversion to Field_real + reverse sort order
    Signed-off-by: Frederic Descamps <lefred@mariadb.org>

--- plugin.cc
+++ plugin.cc
@@ -15,8 +15,10 @@
 
 #include "sql_type_money.h"
 #include "protocol.h"
+#include <float.h>
 #include <mysql/plugin_data_type.h>
 #include <mysql/plugin_function.h>
+#include "filesort.h"
 
 Type_handler_money type_handler_money;
 static constexpr Name type_name={STRING_WITH_LEN("money")};
@@ -62,19 +64,116 @@ void Field_money::make_send_field(Send_field *field)
   field->decimals= 0;
 }
 
+int Field_money::store(const char *from, size_t len, CHARSET_INFO *cs)
+{
+  int error;
+  store(get_double(from, len, cs, &error));
+  return error;
+}
+
+int Field_money::store(double nr)
+{
+  DBUG_ASSERT(marked_for_write_or_computed());
+  int error= truncate_double(&nr, field_length,
+                             not_fixed ? NOT_FIXED_DEC : dec,
+                             unsigned_flag, DBL_MAX);
+  if (unlikely(error))
+  {
+    set_warning(ER_WARN_DATA_OUT_OF_RANGE, 1);
+    if (error < 0)
+    {
+      error= 1;
+      set_null();
+    }
+  }
+
+  float8store(ptr, nr);
+  return error;
+}
+
+int Field_money::store(longlong nr, bool unsigned_val)
+{
+  return store(unsigned_val ? ulonglong2double((ulonglong) nr) : (double) nr);
+}
+
+double Field_money::val_real()
+{
+  DBUG_ASSERT(marked_for_read());
+  double value;
+  float8get(value, ptr);
+  return value;
+}
+
+longlong Field_money::val_int()
+{
+  Converter_double_to_longlong_with_warn conv(get_thd(), val_real(), false);
+  return conv.result();
+}
+
+String *Field_money::val_str(String *val_buffer,
+                             String *val_ptr __attribute__((unused)))
+{
+  DBUG_ASSERT(marked_for_read());
+  DBUG_ASSERT(!zerofill || field_length <= MAX_FIELD_CHARLENGTH);
+
+  const double value= val_real();
+  uint to_length= 128;
+  if (val_buffer->alloc(to_length))
+    return val_buffer;
+
+  char *to= (char *) val_buffer->ptr();
+  size_t len;
+  if (dec >= FLOATING_POINT_DECIMALS)
+    len= my_gcvt(value, MY_GCVT_ARG_DOUBLE, to_length - 1, to, NULL);
+  else
+    len= my_fcvt(value, dec, to, NULL);
+
+  val_buffer->length((uint) len);
+  if (zerofill)
+    prepend_zeros(val_buffer);
+  val_buffer->set_charset(&my_charset_numeric);
+  return val_buffer;
+}
+
 bool Field_money::send(Protocol *protocol)
 {
   DBUG_ASSERT(marked_for_read());
 
   String numeric_buf;
   String money_buf;
-  String *numeric= Field_double::val_str(&numeric_buf, &numeric_buf);
+  String *numeric= val_str(&numeric_buf, &numeric_buf);
 
   money_buf.set_charset(numeric->charset());
   if (money_buf.append('$') || money_buf.append(*numeric))
     return true;
 
   return protocol->store(&money_buf);
+}
+
+int Field_money::cmp(const uchar *a_ptr, const uchar *b_ptr) const
+{
+  double a, b;
+  float8get(a, a_ptr);
+  float8get(b, b_ptr);
+  return (a < b) ? -1 : (a > b) ? 1 : 0;
+}
+
+void Field_money::sort_string(uchar *buff, uint length __attribute__((unused)))
+{
+  double nr;
+  float8get(nr,ptr);
+  change_double_for_sort(nr, buff);
+
+  // Invert key bytes so lexicographic ascending comparison yields descending order.
+  for (uint i= 0; i < sizeof(double); i++)
+    buff[i]^= 0xFF;
+}
+
+Binlog_type_info Field_money::binlog_type_info() const
+{
+  DBUG_ASSERT(Field_money::type() == binlog_type());
+  return Binlog_type_info(Field_money::type(), pack_length(), 1,
+                          binlog_signedness());
 }
 
 static struct st_mariadb_data_type plugin_descriptor_type_money=
@@ -96,7 +195,7 @@ maria_declare_plugin(type_money)
   0x0002,
   NULL,
   NULL,
-  "0.2",
+  "0.3",
   MariaDB_PLUGIN_MATURITY_EXPERIMENTAL
 }
 maria_declare_plugin_end;
--- sql_type_money.h
+++ sql_type_money.h
@@ -40,18 +40,36 @@ public:
 
 extern Type_handler_money type_handler_money;
 
-class Field_money : public Field_double
+class Field_money : public Field_real
 {
 public:
   Field_money(const LEX_CSTRING &name, const Record_addr &addr,
               enum utype unireg_check_arg, uint32 len_arg,
               decimal_digits_t dec_arg, bool zero_arg, bool unsigned_arg)
-    : Field_double(addr.ptr(), len_arg, addr.null_ptr(), addr.null_bit(),
-                   unireg_check_arg, &name, dec_arg, zero_arg, unsigned_arg)
+    : Field_real(addr.ptr(), len_arg, addr.null_ptr(), addr.null_bit(),
+                 unireg_check_arg, &name, fix_dec_arg(dec_arg),
+                 zero_arg, unsigned_arg)
   {}
 
   const Type_handler *type_handler() const override { return &type_handler_money; }
+  enum ha_base_keytype key_type() const override { return HA_KEYTYPE_DOUBLE; }
+  int store(const char *to, size_t length, CHARSET_INFO *charset) override;
+  int store(double nr) override;
+  int store(longlong nr, bool unsigned_val) override;
+  int reset() override { bzero(ptr, sizeof(double)); return 0; }
+  double val_real() override;
+  longlong val_int() override;
+  String *val_str(String *val_buffer, String *val_ptr) override;
   bool send(Protocol *protocol) override;
+  int cmp(const uchar *a_ptr, const uchar *b_ptr) const override;
+  void sort_string(uchar *buff, uint length) override;
+  uint32 pack_length() const override { return sizeof(double); }
+  uint row_pack_length() const override { return pack_length(); }
+  ulonglong get_max_int_value() const override
+  {
+    return 0x20000000000000ULL;
+  }
+  Binlog_type_info binlog_type_info() const override;
   void make_send_field(Send_field *field) override;
 };
 
